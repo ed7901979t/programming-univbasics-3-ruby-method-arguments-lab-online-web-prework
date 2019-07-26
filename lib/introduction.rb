cec284138e0e64ab40cd8eb6a6d0c34ff248aadc
clean-command-1851 <ed7901979t@gmail.com>
2019-07-26 19:27:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,10 +10,8 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   introduction_with_language("Edward", "C++")
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language="Ruby")
 if language=="Ruby"
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
-else
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language_optional("Edward", "Ruby")
